46fb74a3eb2181577952bb1cd6d12ecfecbfff17
user.email <choit@LAPTOP-RVEN208A>
2024-05-02 23:09:35 +0900

    課題1.27未完成

--- curriculum1_27/src/curriculum1_27/animal.java
+++ curriculum1_27/src/curriculum1_27/animal.java
@@ -1,14 +1,65 @@
 package curriculum1_27;
 
 public class animal {
+
 	private String name;
 	private double length;
 	private int speed;
 	private String caption;
 	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCapption(int caaption) {
+		this.caption = caption;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public animal(String[] args) {
-		if (args.length == 3) {
-			int i =0;
+		if(args.length  == 3) {
+			int i = 0;
 			name = args[i++];
 			length = Double.parseDouble(args[i++]);
 			speed = Integer.parseInt(args[i++]);
@@ -16,13 +67,14 @@ public class animal {
 		}
 	}
 	
-	private void createCaption(){
+	private void createCaption() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("動物名：");
 		sb.append(name);
+		sb.append(System.lineSeparator());
 		sb.append("体長：");
 		sb.append(length);
-		sb.append("ｍ");
+		sb.append("m");
 		sb.append(System.lineSeparator());
 		sb.append("速度：");
 		sb.append(speed);
@@ -32,10 +84,8 @@ public class animal {
 		sb.append(ScientificName.getScientificName(name));
 		sb.append(System.lineSeparator());
 		caption = sb.toString();
+		
 	}
 	
-	@Override
-	public String toString() {
-		return caption;
-	}
+	
 }
--- curriculum1_27/src/curriculum1_27/main.java
+++ curriculum1_27/src/curriculum1_27/main.java
@@ -1,12 +1,34 @@
 package curriculum1_27;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class main {
-	private static Scanner scanner = new Scanner(System.in);
-	
 	public static void main(String[] args) {
-		Stream.of(scanner.nextLine().split(",")).map(record -> new animal(args)).forEach(System.out::println);
-}
+		
+		System.out.println("コンソールに文字を入力してください");
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+		
+		List<String> list1 = new ArrayList<>();
+		String input = "ライオン:2.1:80,ゾウ:3.2:40,パンダ:1.9:30,チンパンジー:0.94:25,シマウマ:2.4:65,インコ:0.1:50";
+		
+		//文字列imputを「,」で分割し、配列animalsに格納
+		String[] animals = input.split(",");
+		Collections.addAll(list1,animals);
+		for(String animal:animals) {
+			System.out.println(animal);
+		}
+		
+		animal data = new animal();
+		
+		String n = 
+		
+		
+		data.setName(n);
+		
+		
+	}
 }
